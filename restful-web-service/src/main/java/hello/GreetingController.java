aa3c613dc2feefc7ef65d632a534fb9f983baa77
usman-tahir <tahir.usman.ali94@outlook.com>
2017-06-09 08:55:34 -0400

    added a default route Request Mapping; fixed some extra spacing

--- restful-web-service/src/main/java/hello/GreetingController.java
+++ restful-web-service/src/main/java/hello/GreetingController.java
@@ -12,13 +12,18 @@ public class GreetingController {
 
   private final AtomicLong counter = new AtomicLong();
 
+  @RequestMapping("/")
+  public String home() {
+    return "Hello, RESTful Services!";
+  }
+
   @RequestMapping("/greeting")
   public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-      return new Greeting(counter.incrementAndGet(), String.format(helloTemplate, name));
+    return new Greeting(counter.incrementAndGet(), String.format(helloTemplate, name));
   }
 
   @RequestMapping("/farewell")
   public Farewell farewell(@RequestParam(value="name", defaultValue="World") String name) {
-      return new Farewell(counter.incrementAndGet(), String.format(goodbyeTemplate, name));
+    return new Farewell(counter.incrementAndGet(), String.format(goodbyeTemplate, name));
   }
 }
