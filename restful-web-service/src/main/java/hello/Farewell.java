3ccf121b3e683020cf878f1bc92463b9d1736868
usman-tahir <tahir.usman.ali94@outlook.com>
2017-06-08 22:01:02 -0400

    added a new resource representation class (Farewell) and its resource controller route

---
+++ restful-web-service/src/main/java/hello/Farewell.java
@@ -0,0 +1,8 @@
+package hello;
+
+public class Farewell extends Greeting {
+
+  public Farewell(long id, String content) {
+    super(id, content);
+  }
+}
--- restful-web-service/src/main/java/hello/GreetingController.java
+++ restful-web-service/src/main/java/hello/GreetingController.java
@@ -7,11 +7,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
-  private static final String template = "Hello, %s!";
+  private static final String helloTemplate = "Hello, %s!";
+  private static final String goodbyeTemplate = "Goodbye, %s.";
+
   private final AtomicLong counter = new AtomicLong();
 
   @RequestMapping("/greeting")
   public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-      return new Greeting(counter.incrementAndGet(), String.format(template, name));
+      return new Greeting(counter.incrementAndGet(), String.format(helloTemplate, name));
+  }
+
+  @RequestMapping("/farewell")
+  public Farewell farewell(@RequestParam(value="name", defaultValue="World") String name) {
+      return new Farewell(counter.incrementAndGet(), String.format(goodbyeTemplate, name));
   }
 }
