2a412c861f6eb7639bd1f211e87ae1f700c0337c
usman-tahir <tahir.usman.ali94@outlook.com>
2017-06-11 14:03:59 -0400

    updated the Application class to use in building a .jar deployment

--- file-upload/src/main/java/hello/Application.java
+++ file-upload/src/main/java/hello/Application.java
@@ -1,10 +1,24 @@
 package hello;
 
+import hello.storage.StorageProperties;
+import hello.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class Application {
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+        };
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
