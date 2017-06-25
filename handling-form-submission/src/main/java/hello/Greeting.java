5172828cf02933232948319a51d4e530c8eca87e
Usman Tahir <tahir.usman.ali94@outlook.com>
2017-06-25 09:59:14 -0400

    added a Greeting class, and a GreetingController class (route mapping)

---
+++ handling-form-submission/src/main/java/hello/Greeting.java
@@ -0,0 +1,22 @@
+package hello;
+
+public class Greeting {
+    private long id;
+    private String content;
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
