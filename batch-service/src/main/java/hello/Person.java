a7cf275c9ebfb9fbced3aa9093168606b88a9b32
Usman Tahir <tahir.usman.ali94@outlook.com>
2017-06-25 14:35:26 -0400

    added a gradle build file for the batch processing application; setup the business class and the SQL table definition

---
+++ batch-service/src/main/java/hello/Person.java
@@ -0,0 +1,34 @@
+package hello;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+
+    public Person() {}
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "First name: " + this.getFirstName() + ", Last name: " + this.getLastName() + "\n";
+    }
+}
